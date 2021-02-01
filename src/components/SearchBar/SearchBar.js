e239f3fdbe44148a55e8e3345951273bb5e5b7a1
Armando Rodriguez <arod1207@me.com>
2021-01-31 18:25:48 -0600

    added material ui search field

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { SongContext } from '../../SongContext';
 
+import { TextField } from '@material-ui/core';
+
 import './SearchBar.css';
 
 const SearchBar = () => {
@@ -10,7 +12,10 @@ const SearchBar = () => {
         <div className="searchbar">
             <form onSubmit={searchHandler}>
                 <div className="searchbar__input">
-                    <input
+                    <TextField
+                        id="outlined-basic"
+                        label="Search"
+                        variant="outlined"
                         type="text"
                         placeholder="Search"
                         value={search}
